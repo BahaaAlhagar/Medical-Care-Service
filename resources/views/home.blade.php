a3f04b78d8d540ddf47c3981cb49efc606c35655
BahaaAlhagar <damrawyy@yahoo.com>
2017-08-23 20:53:55 +0200

    dashboard WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts/adminboard')
 
 @section('content')
-<div class="container">
+
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -13,5 +13,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/adminboard.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!-- IE compapility -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- first mobile meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Medical Care Service - @yield('title')</title>
+
+
+        <!-- font awesome -->
+        <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
+
+        <!-- Bootstrap core CSS -->
+        <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="{{ URL::asset('css/adminboard.css') }}" rel="stylesheet">
+
+    @yield('css')
+        <!-- IE 9 -->
+        <script src="{{ URL::asset('js/html5shiv.min.js') }}"></script>
+        <script src="{{ URL::asset('js/respond-1.4.2.min.js') }}"></script>
+
+
+        <!-- jquery -->
+        <script src="{{ URL::asset('js/jquery-3.2.1.slim.min.js') }}"></script>
+        <script src="{{ URL::asset('js/popper.min.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+
+  </head>
+
+    <!-- body starts here -->
+    <body>
+
+        @include('layouts.admin-nav')
+
+        <div class="container">
+        @yield('content')
+
+        @if ($flash = session('message'))
+        <div id="flash" class="alert alert-success" role="alert">
+            
+            <b>{{ $flash }}</b>
+
+        </div>
+        @elseif ($flash = session('error-msg'))
+        <div id="flash" class="alert alert-danger" role="alert">
+            
+            <b>{{ $flash }}</b>
+
+        </div>
+
+        @endif
+
+        </div>
+
+
+
+        @yield('js')
+        <script type="text/javascript">
+            $('#flash').delay(7000).fadeOut("slow");
+        </script>
+
+        <footer class="footer">
+        @yield('footer')
+          <b>&copy; Copyright 2017 by <a href="https://github.com/BahaaAlhagar" target="_blank">Bahaa Alhagar</a>.</b>
+
+        </footer>
+    </body>
+</html>
